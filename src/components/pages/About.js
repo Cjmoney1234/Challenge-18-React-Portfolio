b201b983259de352e9fb8be50394193ed34f0c44
Big_speed90 <zulma.jeff@yahoo.com>
2023-04-10 21:47:50 -0400

    made some changes to style, will continue to make changes but will submit in meantime

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div >
+    <div class="myBio">
       <h1 class="bioTitle">About Me</h1><br></br>
        <p class="bio"><strong>Hello!</strong> My name is Jeff Zulma. I am currently an analyst at a renewable energy company who is in the transition to going public by the summer of 2023.This is really an exciting journey to be a part of as this is the second company I have worked
        for that transitioned from private to public while working their.<br></br><br></br>
-        I plan to use skills I learned from working as an accountant and my time in bootcamp to land me a fufiling position not just financially but emotinally.  During my time 
-          in the bootcamp I plan to update my portfolio often, thank you!          
+        I plan to use skills I learned from working as an accountant, analyst and my time in bootcamp to land me a fufiling position not just financially but emotinally. During my time 
+          in the bootcamp I plan to update my portfolio often, thank you!
       </p>
     </div>
   );
